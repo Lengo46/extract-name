5c89063bdb2531a74f1b34d7d5c7fb4b56674aba
Lengo46 <lengo46@gmail.com>
2023-08-28 11:59:36 +0200

    change in body

--- HttpTrigger1/index.ts
+++ HttpTrigger1/index.ts
@@ -36,8 +36,10 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
 
   context.res = {
     // status: 200, /* Defaults to 200 */
-    result: extractedName,
-    input: inputText,
+    body: {
+      input: inputText,
+      result: extractedName,
+    },
   }
 }
 
